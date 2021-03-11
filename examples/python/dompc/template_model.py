2dd246f4b499174c7c2589a09c3b8715f3c2ff7c
Nicholas Long <nicholas.long@nrel.gov>
2021-03-10 20:05:41 -0700

    use n4sid data

--- examples/python/dompc/template_model.py
+++ examples/python/dompc/template_model.py
@@ -14,39 +14,58 @@ class ModelParameters:
     """This class will be instantiated in various models, so make sure that there are no
     data intensive operations in it."""
     def __init__(self):
+        # Load in the N4SID matrices
+        p = Path('.').resolve().parent / 'lasso_and_n4sid'
+        if p.exists():
+            # States are room temperatures, <to flesh out>
+            self.a = np.load(p / 'matrix_A1.npy')
+            self.b = np.load(p / 'matrix_B1.npy')
+            self.c = np.load(p / 'matrix_C1.npy')
+            self.d = np.load(p / 'matrix_D1.npy')
 
+        print(self.a.shape)
+        print(self.b.shape)
 
-        # # Load in the N4SID matrices
-        # p = Path('.').resolve().parent / 'lasso_and_n4sid'
-        # if p.exists():
-        #     # States are room temperatures, <to flesh out>
-        #     self.a = np.load(p / 'matrix_A1.npy')
-        #     self.b = np.load(p / 'matrix_B1.npy')
-        #     self.c = np.load(p / 'matrix_C1.npy')
-        #     self.d = np.load(p / 'matrix_D1.npy')
-
-        # Example matrices
-        self.a = np.array([[0.763, 0.460, 0.115, 0.020],
-                      [-0.899, 0.763, 0.420, 0.115],
-                      [0.115, 0.020, 0.763, 0.460],
-                      [0.420, 0.115, -0.899, 0.763]])
-
-        self.b = np.array([[0.014],
-                      [0.063],
-                      [0.221],
-                      [0.367]])
-
-        # state space max x values
-        self.max_x = np.array([[4.0], [10.0], [4.0], [10.0]])
+        # TODO: list what these state variables are
+        self.max_x = np.array([
+            [4.0], [10.0], [4.0], [10.0], [4.0], [10.0], [4.0], [10.0]
+        ])
         self.min_x = - self.max_x
 
         # state space max u values
-        self.min_u = -0.5
-        self.max_u = 0.5
+        # TODO: list the control variables here
+        self.max_u = np.array([
+            [4.0], [4.0], [4.0], [4.0], [4.0], [4.0], [4.0], [4.0], [4.0], [4.0], [4.0], [4.0], [4.0], [4.0], [4.0], [4.0], [4.0]
+        ])
+        self.min_u = - self.max_u
 
         # running configuration
         self.time_step = 300
-        self.n_horizon = 7
+        self.n_horizon = 30
+
+        # ------------------------------------------------------------
+        # Example matrices
+        # self.a = np.array([[0.763, 0.460, 0.115, 0.020],
+        #               [-0.899, 0.763, 0.420, 0.115],
+        #               [0.115, 0.020, 0.763, 0.460],
+        #               [0.420, 0.115, -0.899, 0.763]])
+        #
+        # self.b = np.array([[0.014],
+        #               [0.063],
+        #               [0.221],
+        #               [0.367]])
+        #
+        # # state space max x values
+        # self.max_x = np.array([[4.0], [10.0], [4.0], [10.0]])
+        # self.min_x = - self.max_x
+        #
+        # # state space max u values
+        # self.min_u = -0.5
+        # self.max_u = 0.5
+        #
+        # # running configuration
+        # self.time_step = 300
+        # self.n_horizon = 7
 
 
 def template_model():
