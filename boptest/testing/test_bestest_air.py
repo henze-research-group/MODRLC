abafa91f060d208c22adc35524b55d81c4c43591
Matt Robinson <mattrob@hey.com>
2024-02-23 11:22:23 -0700

    Squashed 'boptest-service/' content from commit bca62395
    git-subtree-dir: boptest-service
    git-subtree-split: bca623953338751c3640e29a03d92987d7e5c7fb

---
+++ boptest/testing/test_bestest_air.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+"""
+This module runs tests for bestest_air.  To run these tests, testcase
+bestest_air must already be deployed.
+
+"""
+
+import requests
+import unittest
+import os
+import utilities
+import requests
+
+class Run(unittest.TestCase, utilities.partialTestTimePeriod):
+    '''Tests the example test case.
+
+    '''
+
+    @classmethod
+    def setUpClass(cls):
+        cls.name = 'bestest_air'
+        cls.url = 'http://127.0.0.1:80'
+        cls.testid = requests.post('{0}/testcases/{1}/select'.format(cls.url, cls.name)).json()['testid']
+
+    @classmethod
+    def tearDownClass(cls):
+        requests.put('{0}/stop/{1}'.format(cls.url, cls.testid))
+
+    def setUp(self):
+        '''Setup for each test.
+
+        '''
+
+        self.name = Run.name
+        self.url = Run.url
+        self.points_check = ['fcu_reaPCoo_y', 'fcu_reaFloSup_y',
+                             'fcu_oveTSup_u', 'fcu_oveFan_u',
+                             'zon_reaPPlu_y', 'fcu_reaPFan_y',
+                             'fcu_reaPHea_y', 'zon_reaPLig_y',
+                             'zon_reaTRooAir_y', 'zon_reaCO2RooAir_y',
+                             'zon_weaSta_reaWeaTDryBul_y']
+
+    def test_peak_heat_day(self):
+        self.run_time_period('peak_heat_day')
+
+    def test_peak_cool_day(self):
+        self.run_time_period('peak_cool_day')
+
+    def test_typical_heat_day(self):
+        self.run_time_period('typical_heat_day')
+
+    def test_typical_cool_day(self):
+        self.run_time_period('typical_cool_day')
+
+    def test_mix_day(self):
+        self.run_time_period('mix_day')
+
+    def test_scenario_flag(self):
+        '''Ensures the scenario flag is set properly.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+
+        '''
+
+        length = 86400*14
+        # Get current step
+        step_current = requests.get('{0}/step/{1}'.format(self.url, self.testid)).json()['payload']
+        # Initialize test case scenario
+        requests.put('{0}/scenario/{1}'.format(self.url, self.testid), json={'time_period':'peak_heat_day'})
+        # Set simulation step
+        requests.put('{0}/step/{1}'.format(self.url, self.testid), json={'step':length})
+        # Simulation Loop
+        requests.post('{0}/advance/{1}'.format(self.url, self.testid), json=dict())
+        # Try submit results to dashboard
+        status = requests.post("{0}/submit/{1}".format(self.url, self.testid), json={"api_key": 'valid_key',
+                                                                     "unit_test":"True"}).json()['status']
+        # Check result
+        self.assertEqual(status,200)
+        # Return scenario and step to original
+        requests.put('{0}/step/{1}'.format(self.url, self.testid), json={'step':step_current})
+
+class API(unittest.TestCase, utilities.partialTestAPI):
+    '''Tests the api for testcase.
+
+    Actual test methods implemented in utilities.partialTestAPI.  Set self
+    attributes defined there for particular testcase in setUp method here.
+
+    '''
+
+
+    @classmethod
+    def setUpClass(cls):
+        cls.name = 'bestest_air'
+        cls.url = 'http://127.0.0.1:80'
+        cls.testid = requests.post('{0}/testcases/{1}/select'.format(cls.url, cls.name)).json()['testid']
+
+    @classmethod
+    def tearDownClass(cls):
+        requests.put('{0}/stop/{1}'.format(cls.url, cls.testid))
+
+    def setUp(self):
+        '''Setup for testcase.
+
+        '''
+        self.name = API.name
+        self.url = API.url
+        self.step_ref = 3600.0
+        self.testid = API.testid
+        self.test_time_period = 'peak_heat_day'
+        #<u_variable>_activate is meant to be 0 for the test_advance_false_overwrite API test
+        self.input = {'fcu_oveTSup_activate': 0, 'fcu_oveTSup_u': 290}
+        self.measurement = 'zon_weaSta_reaWeaSolHouAng_y'
+        self.forecast_point = 'EmissionsElectricPower'
+
+if __name__ == '__main__':
+    utilities.run_tests(os.path.basename(__file__))
